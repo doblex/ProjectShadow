329186dd64792248d1310b9f6f8353c42a68850f
Matteo Golfetto <Matteogol16@gmail.com>
2025-12-02 13:43:51 +0100

    Refactoring a evnti dell'actionmanager

--- Assets/Scripts/Manager/ActionManager.cs
+++ Assets/Scripts/Manager/ActionManager.cs
@@ -1,17 +1,38 @@
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class ActionManager : MonoBehaviour
 {
     public static ActionManager Instance;
 
+    public delegate void OnMovementChanged(Vector3 movement);
+    public delegate void OnRotationChanged(Quaternion rotation);
     public delegate void OnInteract();
-    
-    public OnInteract onInteract;
+    public delegate void OnPlayerMovement(Vector2 mousePos, bool dash);
 
-    [SerializeField] private InputActionAsset _action;
+    public OnMovementChanged onMovementChanged;
+    public OnRotationChanged onRotationChanged;
+    public OnInteract onInteract;
+    public OnPlayerMovement onPlayerMovement;
+
+    [SerializeField] private PlayerOptions options;
+    [SerializeField] private GameObject CameraPrefab;
+
+    private InputAction moveVisual;
+    private InputAction rotateVisual;
 
     private InputAction InteractAction;
+
+    private InputAction PlayerMovementAction;
+    private InputAction PlayerDashAction;
+    private InputAction mousePositionAction;
+
+
+    Coroutine MovementCoroutine;
+    Coroutine RotationCoroutine;
 
     private void Awake()
     {
@@ -19,7 +40,7 @@ public class ActionManager : MonoBehaviour
         {
             Destroy(this);
         }
-        else 
+        else
         {
             Instance = this;
             DontDestroyOnLoad(this);
@@ -29,23 +50,137 @@ public class ActionManager : MonoBehaviour
     private void Start()
     {
         SetupCommands();
-    }
 
-    private void Update()
-    {
-        CheckForCommands();
+        if (CameraPrefab != null)
+        { 
+            CameraPrefab.SetActive(true);
+        }
     }
 
     private void SetupCommands()
     {
+        moveVisual = InputSystem.actions.FindAction("MoveCamera");
+        moveVisual.performed += OnMoveDown;
+        moveVisual.canceled += OnMoveUp;
+
+        rotateVisual = InputSystem.actions.FindAction("RotateCamera");
+        rotateVisual.performed += OnRotateDown;
+        rotateVisual.canceled += OnRotateUp;
+
         InteractAction = InputSystem.actions.FindAction("Interact");
+        InteractAction.performed += OnInteractInput;
+
+        PlayerMovementAction = InputSystem.actions.FindAction("MovePlayer");
+        PlayerMovementAction.performed += OnMovePlayer;
+
+        mousePositionAction = InputSystem.actions.FindAction("MousePosition");
     }
 
-    private void CheckForCommands()
+    // -------------------------------
+    //   CALLBACKS
+    // -------------------------------
+
+    private void OnMoveDown(InputAction.CallbackContext ctx)
     {
-        if (InteractAction.IsPressed())
+        Vector2 movement2D = ctx.ReadValue<Vector2>();
+        Vector3 converted = new Vector3(movement2D.x, 0, movement2D.y);
+
+        if (MovementCoroutine != null)
         {
-            onInteract.Invoke();
+            StopCoroutine(MovementCoroutine);
         }
+        MovementCoroutine = StartCoroutine(OnMove(converted));
+    }
+
+    private void OnMoveUp(InputAction.CallbackContext ctx)
+    {
+        Vector2 movement2D = ctx.ReadValue<Vector2>();
+        Vector3 converted = new Vector3(movement2D.x, 0, movement2D.y);
+
+        if (MovementCoroutine != null)
+        { 
+            StopCoroutine(MovementCoroutine);
+        }
+
+        MovementCoroutine = StartCoroutine(OnMove(converted));
+    }
+
+    private IEnumerator OnMove(Vector3 movement)
+    {
+        while (true)
+        {
+            Vector3 move = movement * Time.deltaTime * options.MoveSpeed;
+            onMovementChanged?.Invoke(move);
+            yield return null;
+        }
+    }
+
+
+
+    private void OnRotateDown(InputAction.CallbackContext ctx)
+    {
+        float rotationValue = ctx.ReadValue<float>();
+
+
+        if (RotationCoroutine != null)
+        {
+            StopCoroutine(RotationCoroutine);
+        }
+
+        RotationCoroutine = StartCoroutine(OnRotate(rotationValue));
+    }
+
+    private void OnRotateUp(InputAction.CallbackContext ctx)
+    {
+        float rotationValue = ctx.ReadValue<float>();
+
+
+        if (RotationCoroutine != null)
+        {
+            StopCoroutine(RotationCoroutine);
+        }
+
+        RotationCoroutine = StartCoroutine(OnRotate(rotationValue));
+    }
+
+    private IEnumerator OnRotate(float rotation)
+    {
+        while (true)
+        {
+            Quaternion turn = Quaternion.AngleAxis(
+            rotation * Time.deltaTime * options.AngleSpeed,
+            Vector3.up
+        );
+
+            onRotationChanged?.Invoke(turn);
+            yield return null;
+        }
+    }
+
+    private void OnInteractInput(InputAction.CallbackContext ctx)
+    {
+        onInteract?.Invoke();
+    }
+
+    private void OnMovePlayer(InputAction.CallbackContext ctx)
+    {
+        Vector2 v = mousePositionAction.ReadValue<Vector2>();
+        bool isDoubleClick = false;
+
+        if (ctx.interaction is MultiTapInteraction)
+        { 
+            isDoubleClick = true; 
+        }
+
+        if (isDoubleClick)
+        {
+            Debug.Log("Dash Move");
+        }
+        else
+        {
+            Debug.Log("Normal Move");
+        }
+
+        onPlayerMovement?.Invoke(v, isDoubleClick);
     }
 }
