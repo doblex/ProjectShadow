81413e045b6dca887c2d0a77ce60045a877b88fc
Matteo Golfetto <Matteogol16@gmail.com>
2025-12-11 23:23:58 +0100

    Forced collider on bush

--- Assets/Scripts/Controllers/BushController.cs
+++ Assets/Scripts/Controllers/BushController.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class BushController : MonoBehaviour
 {
+    private void Awake()
+    {
+        Collider sc = GetComponent<Collider>();
+        sc.isTrigger = true;
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
