7fa3eb75692ab1697fb7790d0b63bdded6515ce3
Matteo Golfetto <Matteogol16@gmail.com>
2025-12-12 14:29:23 +0100

    Base UI + controllers

---
+++ Assets/UI/Script/UIEnums.cs
@@ -0,0 +1,22 @@
+
+public enum TransitionType
+{
+    None,
+    Fade,
+    Slide,
+    Scale,
+    Custom
+}
+
+public enum DocumentState
+{
+    Hidden,
+    Visible,
+    Transitioning
+}
+
+public enum DocBehavior
+{
+    single,
+    multiple
+}
---
+++ Assets/UI/Script/UIStructs.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+[Serializable]
+public struct Template
+{
+    public string templateName;
+    public VisualTreeAsset asset;
+}
+
+[Serializable]
+public struct CharacterScript
+{
+
+}
+
+[Serializable]
+public struct Dialogue
+{
+    public string title;
+    public string character;
+    public string text;
+    public string nextTitle;
+    public List<Choice> choices;
+
+    public Dialogue(string title, string character, string text, string nextTitle, List<Choice> choices)
+    {
+        this.title = title;
+        this.character = character;
+        this.text = text;
+        this.nextTitle = nextTitle;
+        this.choices = choices;
+    }
+}
+
+[Serializable]
+public struct Choice
+{
+    public string text;
+    public string nextDialogueTitle;
+
+    public Choice(string text, string nextDialogueTitle)
+    {
+        this.text = text;
+        this.nextDialogueTitle = nextDialogueTitle;
+    }
+}
+
