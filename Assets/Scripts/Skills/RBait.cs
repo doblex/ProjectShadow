c994e8f3731abbe03755793acb286fa6d11d6a5d
Giovanni Faldani <faldanig@yahoo.it>
2025-12-15 00:21:04 +0100

    Small Ability fix

--- Assets/Scripts/Skills/RBait.cs
+++ Assets/Scripts/Skills/RBait.cs
@@ -1,17 +1,15 @@
 using UnityEngine;
+using System.Collections;
 
 public class RBait : MonoBehaviour
 {
     [SerializeField] private NoiseOptions iBaitSound;
 
-    void Start()
-    {
-        
-    }
+    Rigidbody rb;
 
-    void Update()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     public void Despawn()
@@ -22,5 +20,21 @@ public class RBait : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         NoiseSpawnerManager.Instance.SpawnNoiseOrigin(transform.position, iBaitSound);
+        StartCoroutine(StopOnlanding());
+    }
+
+    private IEnumerator StopOnlanding()
+    {
+        float oldDrag = rb.linearDamping;
+        float oldAngularDrag = rb.angularDamping;
+
+        // Se non faccio sta cazzata Unity continua a far rimbalzare la sfera in verticale, non capirò mai sto engine dimmerda
+        rb.linearDamping = 1000f;
+        rb.angularDamping = 1000f;
+
+        yield return new WaitForSeconds(0.5f);
+
+        rb.linearDamping = oldDrag;
+        rb.angularDamping = oldAngularDrag;
     }
 }
