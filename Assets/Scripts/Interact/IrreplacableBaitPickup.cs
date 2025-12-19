104c42ae5cb77110137921fd586c8c105af9fefb
Matteo Golfetto <Matteogol16@gmail.com>
2025-12-19 04:23:16 +0100

    Salvataggio Bait

--- Assets/Scripts/Interact/IrreplacableBaitPickup.cs
+++ Assets/Scripts/Interact/IrreplacableBaitPickup.cs
@@ -1,8 +1,18 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 [RequireComponent(typeof(Highlighter))]
-public class IrreplacableBaitPickup : MonoBehaviour, IInteractable
+public class IrreplacableBaitPickup : MonoBehaviour, IInteractable, ISaveable
 {
+    private string id;
+
+    public string ID => id;
+
+    private void Awake()
+    {
+        id = System.Guid.NewGuid().ToString();
+    }
+
     public void Interact()
     {
         Debug.Log("Interact works");
@@ -13,5 +23,28 @@ public class IrreplacableBaitPickup : MonoBehaviour, IInteractable
             controller.AddIBait();
             Destroy(gameObject);
         }
+    }
+
+    private struct IBaitPickupData
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+    }
+
+    public void Load(string stateJson)
+    {
+        IBaitPickupData data = JsonUtility.FromJson<IBaitPickupData>(stateJson);
+        
+        transform.position = data.position;
+        transform.eulerAngles = data.rotation;
+    }
+
+    public object Save()
+    {
+        return new IBaitPickupData
+        {
+            position = transform.position,
+            rotation = transform.eulerAngles,
+        };
     }
 }
